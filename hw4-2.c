7efb6ad812d1748fc09c80949c40f73632a3fdb4
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-30 20:55:40 +0800

    Update hw4-2.c

--- hw4-2.c
+++ hw4-2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int bank[6][3] = {
+    {123,456,9000},
+    {456,789,5000},
+    {789,888,6000},
+    {336,558,10000},
+    {775,666,12000},
+    {566,221,7000}
+    };
+    int k;
+
+    scanf("%d", &k);
+    int l,m;
+
+
+
+
+
+    for (int j = 0; j < k; j++) {
+        scanf("%d%d",&l,&m);
+        int search[2]={l,m};
+        for(int n=0;n<6;n++){
+            if(search[0]==bank[n][0]&&search[1]==bank[n][1]){
+                printf("%d",bank[n][2]);
+                break;
+            }
+            else if (n==5){
+                printf("error\n");
+            }
+
+        }
+    }
+}
 
