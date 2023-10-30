fdacdb5624dc92e02c4491eabc413636cbbde88b
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-30 19:17:06 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int n,i,a;
+        printf("輸入共有幾個質數:");
+        scanf("%d",&n);
+
+    int numbers[50];
+
+    for(i=0;i<n;i++){
+        scanf("%d/n",&numbers[i]);
+    }
+    for(i=0;i<n;i++){
+        int isprime=1;
+
+        for(a=2;a<numbers[i];a++){
+            if(numbers[i]%a==0){
+                isprime=0;
+                break;
+            }
+        }
+        if(isprime){
+            printf("YES");
+        }
+        else{
+            printf("NO");
+        }
+
+    }
+
+    }
