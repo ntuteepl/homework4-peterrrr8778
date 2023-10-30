2278a73a1c81155e832d691152b2f1941e2baf19
peterrrr8778 <146065025+peterrrr8778@users.noreply.github.com>
2023-10-30 22:01:06 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <assert.h>
+#define ARRAYSIZE 10
 
+main()
+{
+    int n;
+    scanf("%d", &n);
+
+
+    int f[n][n], a, b, max=0, boy, girl ;
+    for (int i = 0; i < n; i++){
+        for(int j=0; j < n; j++){
+        scanf("%d", &(f[i][j]));
+        }
+    }
+
+    for (int k = 0; k < n; k++){
+        max=0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (f[i][j] > max) {
+                    max=f[i][j];
+                    a=i;
+                    b=j;
+                }
+            }
+        }
+        boy=a+1;
+        girl=b+1;
+        printf("boy %d pair with girl %d\n",boy, girl);
+        for (int i = 0; i < n; i++){
+            f[a][i]=0;
+            f[i][b]=0;
+        }
+    }
+}
